d7f09211193c8a35d44b982c215c74a71cdbba11
rcmonitor <rcmonitor@mail.ru>
2015-08-02 14:09:05 +0300

    file logger modified to create file if it possible

--- test/fileLogger.js
+++ test/fileLogger.js
@@ -10,7 +10,7 @@ var fs = require('fs');
 
 var filename = path.join(__dirname, 'log', 'test.log');
 hpg.log(filename, 'filename');
-//var fl = new hpg.fileLogger(filename);
+//var fl = new hpg.FileLogger(filename);
 
 var tracedVar = {
 	"propOne": 42,
@@ -22,12 +22,12 @@ var strInfoMsg = 'some first test message';
 var strWarnMsg = 'any second msg';
 var strErrorMsg = 'more third mess';
 
-function doLog(fileLogger){
+function doLog(FileLogger){
 
-	fileLogger.log(tracedVar, strTraceMsg);
-	fileLogger.log(strInfoMsg, hpg.fileLogger.levels.info);
-	fileLogger.log(strWarnMsg, hpg.fileLogger.levels.warning);
-	fileLogger.log(strErrorMsg, hpg.fileLogger.levels.error);
+	FileLogger.log(tracedVar, strTraceMsg);
+	FileLogger.log(strInfoMsg, hpg.FileLogger.levels.info);
+	FileLogger.log(strWarnMsg, hpg.FileLogger.levels.warning);
+	FileLogger.log(strErrorMsg, hpg.FileLogger.levels.error);
 };
 
 describe('testing file logging:', function(){
@@ -40,12 +40,13 @@ describe('testing file logging:', function(){
 
 	});
 
-	describe('#log() function of fileLogger class', function(){
+	describe('#log() function of FileLogger class', function(){
 		it('should log trace, info, warning and error strings from trace level', function(){
 
-			var fl = new hpg.fileLogger(filename, hpg.fileLogger.levels.trace);
+			var fl = new hpg.FileLogger(filename, hpg.FileLogger.levels.trace);
 
 			doLog(fl);
+			fl.close();
 
 			var loggedStrings = fs.readFileSync(filename, {"encoding": 'utf8'});
 
@@ -59,9 +60,10 @@ describe('testing file logging:', function(){
 
 		it('should only log info, warning and error strings from info level', function(){
 
-			var fl = new hpg.fileLogger(filename);
+			var fl = new hpg.FileLogger(filename);
 
 			doLog(fl);
+			fl.close();
 
 			var loggedStrings = fs.readFileSync(filename, {"encoding": 'utf8'});
 
@@ -76,10 +78,10 @@ describe('testing file logging:', function(){
 		it('should log only warning and error strings from warning log level', function(){
 			var strMsg = 'any second string';
 
-			var fl = new hpg.fileLogger(filename, hpg.fileLogger.levels.warning);
-
+			var fl = new hpg.FileLogger(filename, hpg.FileLogger.levels.warning);
 
 			doLog(fl);
+			fl.close();
 
 			var loggedStrings = fs.readFileSync(filename, {"encoding": 'utf8'});
 
@@ -94,9 +96,10 @@ describe('testing file logging:', function(){
 		it('should log only error string when invoked on error log-level', function(){
 			var strMsg = 'any second string';
 
-			var fl = new hpg.fileLogger(filename, hpg.fileLogger.levels.error);
+			var fl = new hpg.FileLogger(filename, hpg.FileLogger.levels.error);
 
 			doLog(fl);
+			fl.close();
 
 			var loggedStrings = fs.readFileSync(filename, {"encoding": 'utf8'});
 
@@ -106,6 +109,31 @@ describe('testing file logging:', function(){
 			loggedStrings.should.not.contain('info: ' + strInfoMsg);
 			loggedStrings.should.not.contain('warning: ' + strWarnMsg);
 			loggedStrings.should.contain('error: ' + strErrorMsg);
+		});
+
+		it('should log nothing and return error each time we`re trying to log', function(){
+			var strMsg = 'some log message';
+
+			var fl = new hpg.FileLogger('/a/b/c.txt', FileLogger.levels.trace);
+			var strError = fl.error;
+			strError.should.be.a('string');
+			strError.should.contain('no such file or directory');
+
+			strError = fl.log(strMsg, FileLogger.levels.trace);
+			strError.should.be.a('string');
+			strError.should.contain('no such file or directory');
+
+			strError = fl.log(strMsg, FileLogger.levels.info);
+			strError.should.be.a('string');
+			strError.should.contain('no such file or directory');
+
+			strError = fl.log(strMsg, FileLogger.levels.warning);
+			strError.should.be.a('string');
+			strError.should.contain('no such file or directory');
+
+			strError = fl.log(strMsg, FileLogger.levels.error);
+			strError.should.be.a('string');
+			strError.should.contain('no such file or directory');
 		});
 	})
 });
