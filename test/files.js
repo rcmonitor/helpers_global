9ecdb4688483658729e605dcf1fa37e0ae2d3b1f
rcmonitor <rcmonitor@mail.ru>
2015-08-03 11:19:04 +0300

    add ensureFileExistsSync() function
    add ensureFileNotExistsSync() function

---
+++ test/files.js
@@ -0,0 +1,119 @@
+/**
+ * //@by_rcmonitor@//
+ * on 03.08.2015.
+ */
+
+var hpg = require('../index.js');
+var fs = require('fs');
+var path = require('path');
+
+var oPaths = {
+	"relative": './test/data/relative.test'
+	, "absolute": __dirname + path.sep + 'data' + path.sep + 'absolute.test'
+	, "resolved": path.join(__dirname, 'data') + 'resolved.test'
+	, "wrong": '/a/b/c/wrong.test'
+};
+
+describe('filesystem-related tests', function(){
+
+	beforeEach(function(){
+		for(strProperty in oPaths){
+			if(fs.existsSync(oPaths[strProperty])){
+				fs.unlinkSync(oPaths[strProperty]);
+				//console.log(oPaths[strProperty] + ' unlinked before');
+			}
+		}
+	});
+
+	afterEach(function(){
+		for(strProperty in oPaths){
+			if(fs.existsSync(oPaths[strProperty])){
+				fs.unlinkSync(oPaths[strProperty]);
+				//console.log(oPaths[strProperty] + ' unlinked after');
+			}
+		}
+	});
+
+	describe('#ensureFileExistsSync() test', function(){
+		it('should create file in data directory with relative path', function(){
+			hpg.ensureFileExistsSync(oPaths.relative);
+
+			var boolCreated = fs.existsSync(oPaths.relative);
+			boolCreated.should.be.a('boolean');
+			boolCreated.should.equals(true);
+		});
+		it('should create file in data directory with absolute path', function(){
+			hpg.ensureFileExistsSync(oPaths.absolute);
+
+			var boolCreated = fs.existsSync(oPaths.absolute);
+			boolCreated.should.be.a('boolean');
+			boolCreated.should.equals(true);
+		});
+		it('should create file in data directory with resolved path', function(){
+			hpg.ensureFileExistsSync(oPaths.resolved);
+
+			var boolCreated = fs.existsSync(oPaths.resolved);
+			boolCreated.should.be.a('boolean');
+			boolCreated.should.equals(true);
+		});
+		it('should not create file in data directory with wrong path', function(){
+			hpg.ensureFileExistsSync(oPaths.wrong);
+
+			var boolCreated = fs.existsSync(oPaths.wrong);
+			boolCreated.should.be.a('boolean');
+			boolCreated.should.equals(false);
+		});
+	});
+
+
+	describe('#ensureFileNotExistsSync() function tests', function(){
+		
+		var arCorrectPaths = [
+			'./test/data/relative.test'
+			, __dirname + path.sep + 'data' + path.sep + 'absolute.test'
+			, path.join(__dirname, 'data') + 'resolved.test'
+		];
+		
+		var testSuccessfulDeletion = function(strPath){
+			hpg.ensureFileExistsSync(strPath);
+
+			var boolDeleted = hpg.ensureFileNotExistsSync(strPath);
+			boolDeleted.should.be.a('boolean');
+			boolDeleted.should.equals(true);
+
+			var boolExists = fs.existsSync(strPath);
+			boolExists.should.be.a('boolean');
+			boolExists.should.equals(false);
+		};
+
+		arCorrectPaths.forEach(function(strPath){
+			it('should successfully delete file', function(){
+				testSuccessfulDeletion(strPath);
+			})
+		});
+
+
+		var arIncorrectPaths = [
+			'/a/b/c/wrong.test'
+		];
+
+		var testNoDeletion = function(strPath){
+			hpg.ensureFileExistsSync(strPath);
+
+			var boolDeleted = hpg.ensureFileNotExistsSync(strPath);
+			boolDeleted.should.be.a('boolean');
+			boolDeleted.should.equals(false);
+
+			var boolExists = fs.existsSync(strPath);
+			boolExists.should.be.a('boolean');
+			boolExists.should.equals(false);
+		};
+
+		arIncorrectPaths.forEach(function(strPath){
+			it('should not delete inexistent file', function(){
+				testNoDeletion(strPath);
+			});
+		});
+
+	});
+});
