7764f1bdd7dcc78d5688cc8a562233823ef4a574
rcmonitor <rcmonitor@mail.ru>
2015-08-16 10:15:24 +0300

    fix missing parameterObject module
    fix wrong class declarations

--- test/fileLogger.js
+++ test/fileLogger.js
@@ -4,6 +4,7 @@
  */
 
 var hpg = require('../index.js');
+var FileLogger = hpg.FileLogger;
 var path = require('path');
 require('chai').should();
 var fs = require('fs');
@@ -49,7 +50,7 @@ describe('testing file logging:', function(){
 	describe('#log() function of FileLogger class', function(){
 		it('should log trace, info, warning and error strings from trace level', function(){
 
-			var fl = new hpg.FileLogger(filename, hpg.FileLogger.levels.trace);
+			var fl = new FileLogger(filename, FileLogger.levels.trace);
 
 			doLog(fl);
 			fl.close();
@@ -66,7 +67,7 @@ describe('testing file logging:', function(){
 
 		it('should only log info, warning and error strings from info level', function(){
 
-			var fl = new hpg.FileLogger(filename);
+			var fl = new FileLogger(filename);
 
 			doLog(fl);
 			fl.close();
@@ -84,7 +85,7 @@ describe('testing file logging:', function(){
 		it('should log only warning and error strings from warning log level', function(){
 			var strMsg = 'any second string';
 
-			var fl = new hpg.FileLogger(filename, hpg.FileLogger.levels.warning);
+			var fl = new FileLogger(filename, FileLogger.levels.warning);
 
 			doLog(fl);
 			fl.close();
@@ -102,7 +103,7 @@ describe('testing file logging:', function(){
 		it('should log only error string when invoked on error log-level', function(){
 			var strMsg = 'any second string';
 
-			var fl = new hpg.FileLogger(filename, hpg.FileLogger.levels.error);
+			var fl = new FileLogger(filename, FileLogger.levels.error);
 
 			doLog(fl);
 			fl.close();
@@ -120,7 +121,7 @@ describe('testing file logging:', function(){
 		it('should log nothing and return error each time we`re trying to log', function(){
 			var strMsg = 'some log message';
 
-			var fl = new hpg.FileLogger('/a/b/c.txt', FileLogger.levels.trace);
+			var fl = new FileLogger('/a/b/c.txt', FileLogger.levels.trace);
 			var strError = fl.error;
 			strError.should.be.a('string');
 			strError.should.contain('no such file or directory');
