8a1a300918605a94231ba4d2725bff169ca2a977
rcmonitor <rcmonitor@mail.ru>
2015-08-24 14:54:21 +0300

    modify require structure to add codehinting

--- index.js
+++ index.js
@@ -3,6 +3,29 @@
  * on 05.03.2015.
  */
 
-var hpg = require('./lib/hpg');
 
-module.exports = hpg;
+var files = require('./lib/files');
+var types = require('./lib/types');
+var time = require('./lib/time');
+var logger = require('./lib/logger');
+
+var Environment = require('./lib/environment');
+var FileLogger = require('./lib/fileLogger');
+var Configurator = require('./lib/configurator');
+var ParameterObject = require('./lib/parameterObject');
+
+
+exports.log = logger.log;
+exports.time = time.time;
+exports.diff = time.diff;
+exports.getTime = time.getTime;
+exports.getType = types.getType;
+exports.ensureFileExistsSync = files.ensureFileExistsSync;
+exports.ensureFileNotExistsSync = files.ensureFileNotExistsSync;
+exports.ensureDirectoryExistsSync = files.ensureDirectoryExistsSync;
+
+exports.FileLogger = FileLogger;
+exports.Environment = Environment;
+exports.Configurator = Configurator;
+exports.ParameterObject = ParameterObject;
+
