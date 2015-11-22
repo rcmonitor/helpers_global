efa2fd8638f5695fb77a7aae6d5c6ed183d8310d
rcmonitor <rcmonitor@mail.ru>
2015-11-22 14:39:29 +0300

    add month and day i18n functions
    fix wrong time format if seconds < 10 or milliseconds < 100

--- test/time.js
+++ test/time.js
@@ -41,5 +41,33 @@ describe('tests time-related functions of common helper', function(){
 
 			strTime.should.match(/^[\d]{4}\/[\d]{2}\/[\d]{2} [\d]{2}:[\d]{2}:[\d]{2}\.[\d]{3}$/);
 		})
+	});
+
+
+	describe('internationalization', function(){
+		it('should correctly return months', function(){
+			var strRuLongOctober = hpg.getMonthI18N(9);
+			strRuLongOctober.should.equals('Октябрь');
+
+//			var strEnLongDecember = hpg.getMonthI18N(11, "en-US");
+//			strEnLongDecember.should.equals('December');
+
+//			var strRuLongJanuary = hpg.getMonthI18N(0, "ru-RU", "long");
+//			strRuLongJanuary.should.equals('Январь');
+		});
+
+		it('should correctly return days', function(){
+//			var strRuLongSunday = hpg.getDayI18N(0);
+//			strRuLongSunday.should.equals('Воскресенье');
+
+			var strEngLongMonday = hpg.getDayI18N(1, "en-US");
+			strEngLongMonday.should.equals('Monday');
+
+			var strRuShortSaturday = hpg.getDayI18N(6, "ru-RU", "short");
+			strRuShortSaturday.should.equals('Сб');
+
+			var strEnShortWednesday = hpg.getDayI18N(3, "en-US", "short");
+			strEnShortWednesday.should.equals("Wed");
+		})
 	})
 });
